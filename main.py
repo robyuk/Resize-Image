64146405184fb2b99c41b217797852887097480d
robertay <94378311+robyuk@users.noreply.github.com>
2022-09-02 15:46:00 +0000

    modified to resize all files in the images directory

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import cv2
+from pathlib import Path
+
+rootDir=Path('images')
+resizeDir=Path('resized')
+resizePercent=20
 
 def calculateDims(scalePercent, width, height):
   newHeight=int(height*scalePercent/100)
@@ -7,12 +12,17 @@ def calculateDims(scalePercent, width, height):
 
 def resize(imagePath, scalePercent, resizedPath):
   image=cv2.imread(imagePath)
-  print(f'Original Dimensions: {image.shape}')
+  print(f'{imagePath}: Original Dimensions: {image.shape}')
   newDims=calculateDims(scalePercent,image.shape[1],image.shape[0])
   # Note:  image.shape is (h, w, d), 
   # but cv2.resize(image, (w, h)) so h and w are swapped!
   resizedImage=cv2.resize(image, newDims )
-  print(f'New Dimensions: {newDims}')
+  print(f'{resizedPath}: New Dimensions: {newDims}')
   cv2.imwrite(resizedPath, resizedImage)
 
-resize('galaxy.jpeg',20,'resized-galaxy.jpeg')
+filePaths=rootDir.glob("*")
+
+for path in filePaths:
+  if path.is_file():
+    #print(path,f'{resizeDir}/{path.name}')
+    resize(str(path),resizePercent,f'{resizeDir}/{path.name}')
